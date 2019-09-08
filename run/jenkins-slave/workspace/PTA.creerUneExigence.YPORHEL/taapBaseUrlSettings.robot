11d8988dd06f14e5682c0f7d3b5de5cb4ddd255e
Yohann-Porhel <yohann.porhel@laposte.net>
2019-09-09 00:04:15 +0200

    MAJ fichiers

--- run/jenkins-slave/workspace/PTA.creerUneExigence.YPORHEL/taapBaseUrlSettings.robot
+++
@@ -1,9 +0,0 @@
-*** Variables ***
-&{TAAP_PTA}
-...    PROD=https://web1.stevia.enedis.fr/
-
-&{TAAP_SI1}
-...    PROD=https://spice.service-now.com
-
-&{TAAP_PEACH}
-...    IQ=https://mco.mysi.gate-noe.enedis.fr
--- run/workspace/start.bat
+++ run/workspace/start.bat
@@ -5,8 +5,6 @@ set WORKSPACE=%ROBOT_HOME%\run\workspace
 set ROBOT_APP=%ROBOT_HOME%\app
 set ROBOT_RUN=%ROBOT_HOME%\run
 
-set MY_TAAP_BASE_URL_SETTINGS=%WORKSPACE%\taapBaseUrlSettings.robot
-set MY_TAAP_DEVICE_SETTINGS=%WORKSPACE%\taapDeviceSettings.robot
 set MY_DEVICE=PC
 set MY_BROWSER=firefox
 
